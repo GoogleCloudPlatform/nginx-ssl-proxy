7c3b0fb0787215a6be8a936ea9eb7ca08c562c41
Evan Brown <evanbrown@google.com>
2015-04-20 16:34:31 -0700

    SSL or not support

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ RUN rm /etc/nginx/conf.d/*.conf
 WORKDIR /usr/src
 
 ADD start.sh /usr/src/
-ADD *.conf /usr/src/
+ADD nginx/nginx.conf /etc/nginx/
+ADD nginx/proxy*.conf /usr/src/
 
 ENTRYPOINT ./start.sh
--- start.sh
+++ start.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# If SSL key and cert are provided, configure nginx appropriately
+# If SSL key and cert are provided, download and configure nginx
 if [ -n "${SSL_CERT_GCS_URL+1}" ] && [ -n "${SSL_KEY_GCS_URL+1}" ]; then
-  echo "Configuring nginx SSL"
+  echo "Downloading SSL components..."
+  # Download cert and key
   gsutil cp ${SSL_CERT_GCS_URL} /etc/ssl/certs/service-cert
   gsutil cp ${SSL_KEY_GCS_URL} /etc/ssl/private/service-key
+  # Install correct nginx conf
+  cp /usr/src/proxy_ssl.conf /etc/nginx/conf.d/proxy.conf
+else
+  # No SSL
+  cp /usr/src/proxy_nossl.conf /etc/nginx/conf.d/proxy.conf
 fi
 
-sed -i "s/<target_service_host>/${!SERVICE_HOST_ENV_NAME}:${!SERVICE_PORT_ENV_NAME}/g;" /usr/src/target_service.conf
-cp /usr/src/target_service.conf /etc/nginx/conf.d/target_service.conf
+# If an htpasswd file is provided, download and configure nginx 
+if [ -n "${HTPASSWD_GCS_URL+1}" ]; then
+  echo "Downloading htpasswd components..."
+   gsutil cp ${HTPASSWD_GCS_URL} /etc/nginx/target_service.htpasswd
+   sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/proxy.conf
+fi
+
+# Tell nginx the address and port of the service to proxy to
+sed -i "s/{{TARGET_SERVICE_HOST}}/${!SERVICE_HOST_ENV_NAME}:${!SERVICE_PORT_ENV_NAME}/g;" /etc/nginx/conf.d/proxy.conf
+
+echo "Starting nginx..."
 nginx -g 'daemon off;'
