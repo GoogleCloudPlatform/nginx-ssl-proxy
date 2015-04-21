5223512feee27f4080295a12aa04c5c3253ea31e
Evan Brown <evanbrown@google.com>
2015-04-21 15:22:38 -0700

    Retrieve secrets from vol mounts, no gcloud required

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,5 @@
 FROM nginx
 
-# Install gcloud
-ENV CLOUDSDK_PYTHON_SITEPACKAGES 1
-RUN apt-get update -y && apt-get install -y -qq --no-install-recommends wget unzip python \ 
-  && apt-get clean \
-  && cd $HOME \
-  && wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip \
-  && google-cloud-sdk/install.sh --usage-reporting=true --path-update=true --bash-completion=true --rc-path=/.bashrc --disable-installation-options \
-  && google-cloud-sdk/bin/gcloud --quiet config set component_manager/disable_update_check true
-ENV PATH /root/google-cloud-sdk/bin:$PATH
-
 RUN rm /etc/nginx/conf.d/*.conf
 
 WORKDIR /usr/src
--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# If SSL key and cert are provided, download and configure nginx
-if [ -n "${SSL_CERT_GCS_URL+1}" ] && [ -n "${SSL_KEY_GCS_URL+1}" ]; then
-  echo "Downloading SSL components..."
-  # Download cert and key
-  gsutil cp ${SSL_CERT_GCS_URL} /etc/ssl/certs/service-cert
-  gsutil cp ${SSL_KEY_GCS_URL} /etc/ssl/private/service-key
-  # Install correct nginx conf
+# Env says we're using SSL 
+if [ -n "${ENABLE_SSL+1}" ] && [ "${ENABLE_SSL,,}" = "true" ]; then
+  echo "Enabling SSL..."
   cp /usr/src/proxy_ssl.conf /etc/nginx/conf.d/proxy.conf
 else
   # No SSL
@@ -14,9 +10,8 @@ else
 fi
 
 # If an htpasswd file is provided, download and configure nginx 
-if [ -n "${HTPASSWD_GCS_URL+1}" ]; then
-  echo "Downloading htpasswd components..."
-   gsutil cp ${HTPASSWD_GCS_URL} /etc/nginx/target_service.htpasswd
+if [ -n "${ENABLE_BASIC_AUTH+1}" ] && [ "${ENABLE_BASIC_AUTH,,}" = "true" ]; then
+  echo "Enabling basic auth..."
    sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/proxy.conf
 fi
 
