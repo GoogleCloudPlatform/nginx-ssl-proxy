b5b8153aca1525fbbff3e824b216130b5fc0454d
Evan Brown <evanbrown@google.com>
2015-05-21 14:50:35 -0700

    Fix typo in env var name

--- start.sh
+++ start.sh
@@ -30,11 +30,11 @@ fi
 # If the SERVICE_HOST_ENV_NAME and SERVICE_PORT_ENV_NAME vars are provided,
 # they point to the env vars set by Kubernetes that contain the actual
 # target address and port. Override the default with them.
-if [ -n "${SERVICE_HOST_ENV_NAME+1}" ] && [ "${SERVICE_HOST_ENV_NAME,,}" = "true" ]; then
+if [ -n "${SERVICE_HOST_ENV_NAME+1}" ]; then
   TARGET_SERVICE=${!SERVICE_HOST_ENV_NAME}
 fi
-if [ -n "${SERVICE_PORT_ENV_NAME+1}" ] && [ "${SERVICE_PORT_ENV_NAME,,}" = "true" ]; then
-  TARGET_SERVICE="$PROXY_TARGET_HOST${!SERVICE_PORT_ENV_NAME}":
+if [ -n "${SERVICE_PORT_ENV_NAME+1}" ]; then
+  TARGET_SERVICE="$TARGET_SERVICE:${!SERVICE_PORT_ENV_NAME}"
 fi
 
 # Tell nginx the address and port of the service to proxy to
