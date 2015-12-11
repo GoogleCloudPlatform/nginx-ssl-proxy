36a31fb8f08c0d6522b26afbf8bfbc87ee05847c
Alex Couper <amcouper@gmail.com>
2015-12-11 13:57:59 +0000

    Route through to cert container.

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
-# Env says we're using SSL 
+# Env says we're using SSL
 if [ -n "${ENABLE_SSL+1}" ] && [ "${ENABLE_SSL,,}" = "true" ]; then
   echo "Enabling SSL..."
   cp /usr/src/proxy_ssl.conf /etc/nginx/conf.d/proxy.conf
@@ -21,7 +21,7 @@ else
   cp /usr/src/proxy_nossl.conf /etc/nginx/conf.d/proxy.conf
 fi
 
-# If an htpasswd file is provided, download and configure nginx 
+# If an htpasswd file is provided, download and configure nginx
 if [ -n "${ENABLE_BASIC_AUTH+1}" ] && [ "${ENABLE_BASIC_AUTH,,}" = "true" ]; then
   echo "Enabling basic auth..."
    sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/proxy.conf
@@ -37,8 +37,22 @@ if [ -n "${SERVICE_PORT_ENV_NAME+1}" ]; then
   TARGET_SERVICE="$TARGET_SERVICE:${!SERVICE_PORT_ENV_NAME}"
 fi
 
+# If the CERT_SERVICE_HOST_ENV_NAME and CERT_SERVICE_PORT_ENV_NAME vars
+# are provided, they point to the env vars set by Kubernetes that contain the
+# actual target address and port of the encryption service. Override the
+# default with them.
+if [ -n "${CERT_SERVICE_HOST_ENV_NAME+1}" ]; then
+  CERT_SERVICE=${!CERT_SERVICE_HOST_ENV_NAME}
+fi
+if [ -n "${CERT_SERVICE_PORT_ENV_NAME+1}" ]; then
+  CERT_SERVICE="$CERT_SERVICE:${!CERT_SERVICE_PORT_ENV_NAME}"
+fi
+
 # Tell nginx the address and port of the service to proxy to
 sed -i "s/{{TARGET_SERVICE}}/${TARGET_SERVICE}/g;" /etc/nginx/conf.d/proxy.conf
+# Tell nginx the address and port of the certification service.
+sed -i "s/{{CERT_SERVICE}}/${CERT_SERVICE}/g;" /etc/nginx/conf.d/proxy.conf
 
 echo "Starting nginx..."
+cat /etc/nginx/conf.d/proxy.conf
 nginx -g 'daemon off;'
